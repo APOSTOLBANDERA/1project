33cc37b04f92ccd7edeb1d37cf980faa90a11c2e
APOSTOLBANDERA <APOSTOLBANDERA>
2022-09-24 12:00:02 +0300

    Start

---
+++ Manager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Manager : Worker
+    {
+        public int ClientsCount { get; set; }
+
+        public Manager(string name, int age, double salary, int clientsCount) 
+            : base(name, age, salary)
+        {
+            Console.WriteLine("Manager full constructor");
+            ClientsCount = clientsCount;
+        }
+        public Manager() 
+            : base()
+        {
+
+        }
+        public override double GetResultSalary()
+        {
+            return Salary * ClientsCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp10;
+
+
+Manager manager = new Manager();
+Manager manager1 = new Manager("Gnip", 23, 5000, 6);
+
